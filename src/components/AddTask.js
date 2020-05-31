1d7fd9bf8be745da7897c94ee676800c912d84d7
aaryamNUS <aaryamps1@gmail.com>
2020-05-31 12:17:10 +0800

    fixed bug with add task feature

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -44,7 +44,7 @@ export const AddTask = ({
           projectId,
           task,
           date: collatedDate || taskDate,
-          userId: 'jlIFXIwyAL3tzHMtzRbw',
+          userID: 'cookiez253',
         })
         .then(() => {
           setTask('');
